b865d085c762c4aa1f7856ee9f51cd81fa6e26f5
Alban <albanstudios1@gmail.com>
2024-06-04 01:18:44 +0200

    Added Hamming Distance function

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Diagnostics;
+
 namespace ReedSolomon
 {
     internal static class Program
@@ -10,6 +12,20 @@ namespace ReedSolomon
             var decodedMessage = codedMessage / coder.GetGenerator;
             var str = decodedMessage.PolyToBits();
             Console.WriteLine($"Decoded message in bits: {str}");
+
+            int[] vectorA = [1, 2, 3, 4];
+            int[] vectorB = [5, 2, 3, 4];
+            var distance = HammingDistance(vectorA, vectorB);
+            
+            Console.WriteLine($"Hamming Distance between {vectorA.Aggregate("",(current, item) => current + item + " ")} ({vectorA.Aggregate("",(current, item) => current + $"{item:b5}" + " ")}) and {vectorB.Aggregate("",(current, item) => current + item + " ")} ({vectorB.Aggregate("",(current, item) => current + $"{item:b5}" + " ")}) is {distance.Aggregate("", (current, item) => current + item + " ")}");
+            
+        }
+        static IEnumerable<int> HammingDistance(IEnumerable<int> a, IEnumerable<int> b) { // funkcja liczy dystans Hamminga między odpowiednimi liczbami w dwóch tablicach 
+            var arrays = a.Zip(b, (first, second) => new { a = first, b = second });
+            foreach (var pair in arrays)
+            {
+                yield return int.PopCount(pair.a ^ pair.b); //PopCount zlicza ustawione bity maski uzyskanej przez działanie, a następnie zwraca wynik
+            }
         }
     }
 }
